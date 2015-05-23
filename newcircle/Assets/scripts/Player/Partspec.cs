9292731eaa8872b9070e2f434aa8bc1ad8ce8db6
sirerr <sirerr@gmail.com>
2015-05-23 18:37:52 -0400

    quick upload

---
+++ newcircle/Assets/scripts/Player/Partspec.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Partspec : MonoBehaviour {
+
+	//weight of part
+	public float pweight = 0;
+	//does it take power from ship?
+	public bool powerusage = false;
+	public float ppowerneeded = 0;
+
+	//specific class
+	public bool classspecific = false;
+	public string partclass = "universal";
+
+	//speed inc
+	public bool incspeed = false;
+	public float addedspeed = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- newcircle/Assets/scripts/Player/shipspec.cs
+++ newcircle/Assets/scripts/Player/shipspec.cs
@@ -4,15 +4,44 @@ using System.Collections.Generic;
 
 public class shipspec : MonoBehaviour {
 
+	//health
 	public float shiphealth = 0;
+	public float defaultshiphealth = 0;
+	//power
+	public float shippower = 0;
+	public float defaultshippower = 0;
+	public float shippowertaken = 0;
+	//ship info
 	public string shipname;
 	public string shipclass;
-	public float shipweight =0;
+
+	//speed inc
+	public float shipspeedinc;
+
+	//weight
+	public float shipweightadd =0;
+	public float defaultshipweight = 0;
+
 	public List<GameObject> shipadds = new List<GameObject>();
+	public List<GameObject> enginepart = new List<GameObject>();
+	public List<GameObject> bodypart = new List<GameObject>();
+	public List<GameObject> specialpart = new List<GameObject>();
+	public List<GameObject> weaponpart = new List<GameObject>();
+
+	// part attached
+	public List<bool> partattached = new List<bool>();
+	// ship gameobj
 	public GameObject shipbaseobj;
  
 	// Use this for initialization
 	void Start () {
+
+
+		// set default values at beginning of game
+		defaultshiphealth = shiphealth;
+		defaultshippower = shippower;
+
+		//
 		shipbaseobj = transform.GetChild(0).gameObject;
 
 		for(int i =0;i<shipbaseobj.transform.childCount;i++)
@@ -22,11 +51,84 @@ public class shipspec : MonoBehaviour {
 				shipadds.Add(shipbaseobj.transform.GetChild(i).gameObject);
 			}
 		}
+		//check to see if any ship add locations have a part attached, 
 
+		for( int a =0;a< shipadds.Count;a++)
+		{
+ 			
+
+
+			GameObject temppart = shipadds[a].transform.GetChild(0).gameObject;
+
+			if(temppart.activeSelf)
+			{
+
+			switch (temppart.tag)
+			{
+			case "enginepart":
+				print ("I got a engine part");
+
+				enginepart.Add (temppart);
+				partattached.Add(true);
+
+				if(temppart.GetComponent<Partspec>().pweight > 0)
+				{
+					shipweightadd += temppart.GetComponent<Partspec>().pweight;
+				}
+				if(temppart.GetComponent<Partspec>().powerusage)
+				{
+					shippowertaken += temppart.GetComponent<Partspec>().ppowerneeded;
+				}
+				if(temppart.GetComponent<Partspec>().incspeed)
+				{
+					shipspeedinc += temppart.GetComponent<Partspec>().addedspeed;
+				}
+				break;
+			case "bodypart":
+				print ("I got a bodypart");
+				bodypart.Add(temppart);
+				partattached.Add(true);
+				if(temppart.GetComponent<Partspec>().pweight > 0)
+				{
+					shipweightadd += temppart.GetComponent<Partspec>().pweight;
+				}
+
+				break;
+			case "specialpart":
+				print ("I got a special part");
+
+				specialpart.Add(temppart);
+				partattached.Add(true);
+				if(temppart.GetComponent<Partspec>().powerusage)
+				{
+					shippowertaken += temppart.GetComponent<Partspec>().ppowerneeded;
+				}
+
+				break;
+			case "weaponpart":
+
+				weaponpart.Add(temppart);
+				partattached.Add(true);
+				if(temppart.GetComponent<Partspec>().pweight > 0)
+				{
+					shipweightadd += temppart.GetComponent<Partspec>().pweight;
+				}
+				if(temppart.GetComponent<Partspec>().powerusage)
+				{
+					shippowertaken += temppart.GetComponent<Partspec>().ppowerneeded;
+				}
+
+				print ("I got a weapon part");
+				break;
+				}
+			}
+		}
+
+		transform.GetComponent<playercontrol>().incspeed += shipspeedinc;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
+	 }
 }
