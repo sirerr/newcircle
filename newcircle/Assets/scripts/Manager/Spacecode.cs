d71682fa693f72fcec60ae208834e815085a1266
sirerr <sirerr@gmail.com>
2015-05-25 21:16:40 -0400

    made quick cube enemy

---
+++ newcircle/Assets/scripts/Manager/Spacecode.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spacecode : MonoBehaviour {
+
+	// The idea of this scritp is to handle the creation of the arena 
+
+	public GameObject arenacenter;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(arenacenter,transform.transform.position,transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
